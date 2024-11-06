c0b59818495e9d32ad5bf7dddf7145665df13fd8
KIM YU BIN <55120730+KimYooBin1@users.noreply.github.com>
2024-11-06 11:04:17 +0900

    [Fix]: Update second-algorithm.js

--- common/second-algorithm.js
+++ common/second-algorithm.js
@@ -11,13 +11,13 @@ function isEmpty(value) {
 
     // 배열
     if (Array.isArray(value)) {
-        // 모든 요소가 확인
+        // 모든 요소 확인
         return value.every(isEmpty);
     }
 
     // 객체
     if (typeof value === "object") {
-        // 모든 속성 속성, 키 확인
+        // 모든 속성, 키 확인
         return Object.keys(value).length === 0 || Object.values(value).every(isEmpty);
     }
 
